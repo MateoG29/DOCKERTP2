bb3e7e62bc5eccb794bee368d3f5f06d2cd1e084
Ubuntu <adminmg@VMFLASK.eevxf4yrmguu1j0ygillwlwdec.bx.internal.cloudapp.net>
2024-04-04 11:57:50 +0000

    Version DevOps

---
+++ test_app.py
@@ -0,0 +1,8 @@
+# test_app.py
+
+from app import app as flask_app
+
+def test_home():
+    response = flask_app.test_client().get('/')
+    assert response.status_code == 200
+    assert "Bonjour!" in response.data.decode()
